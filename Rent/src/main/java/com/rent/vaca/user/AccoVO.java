833f5d5064215341375099c4c098c06fe46b1a58
MYCOM <MYCOM@DESKTOP-HDNEVN7>
2025-09-10 14:30:39 +0900

    yms - VO추가, resources/img 샘플 이미지 추가.
    Merge branch 'main' of https://github.com/2Miso/vacationRent.git into
    main

---
+++ Rent/src/main/java/com/rent/vaca/user/AccoVO.java
@@ -0,0 +1,83 @@
+package com.rent.vaca.user;
+
+public class AccoVO {
+	private int accoNo;
+	private int type;
+	private String name;
+	private String addr;
+	private String phone;
+	private String description;
+	private String bizHour;
+	private String delyn;
+	private String checkin;
+	private String checkout;
+	private int bizId;
+	public int getAccoNo() {
+		return accoNo;
+	}
+	public void setAccoNo(int accoNo) {
+		this.accoNo = accoNo;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBizHour() {
+		return bizHour;
+	}
+	public void setBizHour(String bizHour) {
+		this.bizHour = bizHour;
+	}
+	public String getDelyn() {
+		return delyn;
+	}
+	public void setDelyn(String delyn) {
+		this.delyn = delyn;
+	}
+	public String getCheckin() {
+		return checkin;
+	}
+	public void setCheckin(String checkin) {
+		this.checkin = checkin;
+	}
+	public String getCheckout() {
+		return checkout;
+	}
+	public void setCheckout(String checkout) {
+		this.checkout = checkout;
+	}
+	public int getBizId() {
+		return bizId;
+	}
+	public void setBizId(int bizId) {
+		this.bizId = bizId;
+	}
+	
+	
+}

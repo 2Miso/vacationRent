814dc94b0203f8db90ec2d5a816bf8570cbdffd1
MYCOM <MYCOM@DESKTOP-2U7HOS2>
2025-09-10 11:54:37 +0900

    Merge branch 'main' of https://github.com/2Miso/vacationRent.git into main

---
+++ Rent/src/main/java/com/rent/vaca/user/NoticeVO.java
@@ -0,0 +1,68 @@
+package com.rent.vaca.user;
+
+public class NoticeVO {
+	private int noticeNo;
+	private String type;
+	private String title;
+	private String content;
+	private String wdate;
+	private String delyn;
+	private String answeryn;
+	private String answerContent;
+	private String answerDate;
+	
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWdate() {
+		return wdate;
+	}
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+	public String getDelyn() {
+		return delyn;
+	}
+	public void setDelyn(String delyn) {
+		this.delyn = delyn;
+	}
+	public String getAnsweryn() {
+		return answeryn;
+	}
+	public void setAnsweryn(String answeryn) {
+		this.answeryn = answeryn;
+	}
+	public String getAnswerContent() {
+		return answerContent;
+	}
+	public void setAnswerContent(String answerContent) {
+		this.answerContent = answerContent;
+	}
+	public String getAnswerDate() {
+		return answerDate;
+	}
+	public void setAnswerDate(String answerDate) {
+		this.answerDate = answerDate;
+	}
+}
---
+++ Rent/src/main/java/com/rent/vaca/user/ReviewVO.java
@@ -0,0 +1,40 @@
+package com.rent.vaca.user;
+
+public class ReviewVO {
+	private int reviewNo;
+	private int star;
+	private String content;
+	private String wdate;
+	private String reply;
+	
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWdate() {
+		return wdate;
+	}
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+}

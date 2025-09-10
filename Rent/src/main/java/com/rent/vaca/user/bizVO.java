1a9f986b5b17387bc55be5617da6c968432aaa59
MYCOM <MYCOM@DESKTOP-CBJAQV5>
2025-09-10 12:34:52 +0900

    html파일추가 , VO (biz,room,reviewphoto,interest)추가 - ghw(2)

---
+++ Rent/src/main/java/com/rent/vaca/user/bizVO.java
@@ -0,0 +1,104 @@
+package com.rent.vaca.user;
+
+public class bizVO{
+	private int id;	
+	private String email;
+	private String pw;
+	private String phone;
+	private String wdate;
+	private String approveyn;
+	private String delyn;
+	private String name;
+	private String owner;
+	private String certificateNo;
+	private String certificateOriginalName;
+	private String certificateSavedName;
+	private String banyn;
+	private String banReason;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getWdate() {
+		return wdate;
+	}
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+	public String getApproveyn() {
+		return approveyn;
+	}
+	public void setApproveyn(String approveyn) {
+		this.approveyn = approveyn;
+	}
+	public String getDelyn() {
+		return delyn;
+	}
+	public void setDelyn(String delyn) {
+		this.delyn = delyn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getCertificateNo() {
+		return certificateNo;
+	}
+	public void setCertificateNo(String certificateNo) {
+		this.certificateNo = certificateNo;
+	}
+	public String getCertificateOriginalName() {
+		return certificateOriginalName;
+	}
+	public void setCertificateOriginalName(String certificateOriginalName) {
+		this.certificateOriginalName = certificateOriginalName;
+	}
+	public String getCertificateSavedName() {
+		return certificateSavedName;
+	}
+	public void setCertificateSavedName(String certificateSavedName) {
+		this.certificateSavedName = certificateSavedName;
+	}
+	public String getBanyn() {
+		return banyn;
+	}
+	public void setBanyn(String banyn) {
+		this.banyn = banyn;
+	}
+	public String getBanReason() {
+		return banReason;
+	}
+	public void setBanReason(String banReason) {
+		this.banReason = banReason;
+	}
+
+}

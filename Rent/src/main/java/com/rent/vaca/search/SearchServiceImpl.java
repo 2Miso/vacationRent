ea49edc1414626e5bf08bfcd144855831503cfd3
MYCOM <MYCOM@DESKTOP-HDNEVN7>
2025-09-22 09:02:05 +0900

    yms - 에러 수정

--- Rent/src/main/java/com/rent/vaca/search/SearchServiceImpl.java
+++ Rent/src/main/java/com/rent/vaca/search/SearchServiceImpl.java
@@ -3,10 +3,12 @@ package com.rent.vaca.search;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rent.vaca.acco.AccoRepository;
 import com.rent.vaca.acco.AccoVO;
 
+@Service
 public class SearchServiceImpl implements SearchService{
 
 	private final AccoRepository accoRepository;
